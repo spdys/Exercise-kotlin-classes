095782d8ab89921f8d2e2a8ef0729a8692594a81
spdys <noorii549@yahoo.com>
2025-02-16 18:36:16 +0300

    Added genre according to the challenge section

--- src/classBook.kt
+++ src/classBook.kt
@@ -1,10 +1,10 @@
-class Book(var title: String, var author: String, var yearPublished: Int) {
+class Book(var title: String, var author: String, var yearPublished: Int, var genre: String) {
     fun getBookInfo() {
-        println("$title by $author ($yearPublished)")
+        println("$title by $author ($yearPublished), $genre")
     }
 }
 
 fun main() {
-    val myBook = Book("Annie Bot", "Sierra Greer", 2024);
+    val myBook = Book("Annie Bot", "Sierra Greer", 2024, "Science Fiction");
     myBook.getBookInfo()
 }
